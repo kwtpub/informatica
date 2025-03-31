84e2ff3641862ebf054c42fdde43e33b11300f60
kwtpub <10.0.kwt@mail.ru>
2025-03-31 16:53:47 -0700

    add v3

--- Варианты/3 Вариант/питухон/17.py
+++ Варианты/3 Вариант/питухон/17.py
@@ -1,6 +1,10 @@
 n = [int(x) for x in open('17var03.txt')]
 
+minN = min(n)
+ans = []
+for i in range(1,len(n)-1):
+    if ((n[i] % 44) + (n[i+1] % 44)) == minN:
+        ans.append(abs(n[i] - n[i-1]))
 
-for i in range(1,len(n)+1):
-
+print(len(ans), min(ans))
 
